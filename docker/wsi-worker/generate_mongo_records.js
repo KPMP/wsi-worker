b4ad07557f7fd07f99f5c13fe154cea1f3642e02
Becky Reamy <rlreamy@umich.edu>
2019-11-14 16:08:19 -0500

    KPMP-1419: Picking up missed files from previous commit.  Can add new slide to existing participant

---
+++ docker/wsi-worker/generate_mongo_records.js
@@ -0,0 +1,48 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://delphinus-mongodb:27017';
+const dbName = 'knowledgeEnvironment';
+
+var kpmpId = process.argv[2];
+var slideName = process.argv[3];
+var fileUUID = process.argv[4];
+var udpates = [];
+
+const createUpdates = function(db, callback) {
+	var participantCollection = db.collection("patients");
+	var query = { kpmp_id: kpmpId };
+
+	participantCollection.find(query).toArray(function(err, docs) {
+		assert.equal(null, err);
+		docs.forEach(function(doc) {
+			var slides = doc.slides;
+			var exists = false;
+			slides.forEach(slide => {
+				if(slide.slideName === slideName) {
+					exists = true;
+				}
+			});
+			if (!exists) {
+				console.log("adding new slide")
+				// Not adding the stain type because we aren't using in DPR
+				slides.push({ _id: fileUUID, slideName: slideName });
+				participantCollection.update({ _id: doc._id }, { $set: { slides: slides }});
+			}
+		});
+		callback();
+	});
+
+}
+
+
+MongoClient.connect(url, function(err, client) {
+	assert.equal(null, err);
+	
+	const db = client.db(dbName);
+	
+	createUpdates(db, function() {
+		client.close();
+	});
+
+});
