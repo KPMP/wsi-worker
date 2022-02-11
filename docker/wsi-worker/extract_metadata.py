7b12b0d3c4ca087103243e3fbdcf047dfb05c4f8
Becky Reamy <rlreamy@umich.edu>
2022-02-11 16:19:21 -0500

    KPMP-3104: Calculating overlay values

--- docker/wsi-worker/extract_metadata.py
+++ docker/wsi-worker/extract_metadata.py
@@ -1,8 +1,71 @@
 import openslide
 import sys
 import json
+import math
 
-print("\n ".join(sys.argv))
+def getCurrentLetter(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+def calculateGrids(metadata):
+    verticalSize = 500
+    horizontalSize = 500
+    lineThickness = 13
+    vertical = verticalSize / float(metadata['openslide']['mpp_y']);
+    horizontal = horizontalSize / float(metadata['openslide']['mpp_y']);
+    overlay = []
+    overlayLabel = []
+    if ((metadata != None) and metadata['aperio'] and metadata['aperio']['OriginalHeight'] != None and metadata['aperio']['OriginalWidth'] != None):
+        width = int(metadata['aperio']['OriginalWidth'])
+        height = int(metadata['aperio']['OriginalHeight'])
+        gridLineLengthForHeight = (math.ceil(((height + horizontal) / horizontal)) - 1) * horizontal;
+        gridLineLengthForWidth = (math.ceil(((width + horizontal) / horizontal)) - 1) * horizontal;
+
+        i = 0.0
+        while (i <= (width + vertical)):
+            overlay.append({
+                'px': 0,
+                'py': i,
+                'width': lineThickness,
+                'height': gridLineLengthForHeight,
+                'className': 'gridline'
+            })
+            i += vertical
+        j = 0.0
+        while (j <= (height + horizontal)):
+            overlay.append({
+                'px': 0,
+                'py': j,
+                'width': gridLineLengthForWidth,
+                'height': lineThickness,
+                'className': 'gridline'
+            })
+            j += horizontal
+
+    currentNumber = 0
+    yy = 0
+    while (yy < (height)):
+        currentLetterCount = 1
+        currentLetter = getCurrentLetter(currentLetterCount)
+        i=0
+        while (i < (width)):
+            overlayLabel.append(currentLetter + str(currentNumber))
+            overlay.append({
+                'id': 'labelOverlay-' + currentLetter + str(currentNumber),
+                'px': 0 + (i/vertical * vertical + lineThickness),
+                'py': 0 + (yy/horizontal * horizontal + lineThickness),
+            })
+            i+= vertical
+            currentLetterCount += 1
+            currentLetter = getCurrentLetter(currentLetterCount)
+        yy += vertical
+        currentNumber += 1
+    print(overlayLabel)
+    print(overlay)
+
 
 svsfile = sys.argv[1]
 metadata_out = sys.argv[2]
@@ -21,8 +84,10 @@ for properties in slide.properties:
         else:
             metadata[mainKey] = {subkey: slide.properties[properties]}
 
-print('metadata_out', metadata_out)
+# print('metadata_out', metadata_out)
 with open(metadata_out, 'w') as metadata_file:
     metadata_file.write(json.dumps(metadata))
 
-slide.close()
+slide.close();
+calculateGrids(metadata)
+
