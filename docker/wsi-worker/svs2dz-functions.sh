ff21c0f1f770d9c3e6f14e29033b3fe894d3d7a0
rlreamy <34109594+rlreamy@users.noreply.github.com>
2024-08-22 13:32:32 -0400

    Update svs2dz-functions.sh

--- docker/wsi-worker/svs2dz-functions.sh
+++ docker/wsi-worker/svs2dz-functions.sh
@@ -70,8 +70,8 @@ validate_args() {
 call_vips() {
   mkdir -p $ENV_LINK_SRC_DIR/files_$1
   if [ "$4" == "EM" ] || [ "$4" == "em" ]; then
-    echo "--vips dsave $JOB_IN_DIR/$2.jpg $ENV_LINK_SRC_DIR/files_$1/$2"
-    vips dsave $JOB_IN_DIR/$2.jpg $ENV_LINK_SRC_DIR/files_$1/$2
+    echo "--vips dzsave $JOB_IN_DIR/$2.jpg $ENV_LINK_SRC_DIR/files_$1/$2"
+    vips dzsave $JOB_IN_DIR/$2.jpg $ENV_LINK_SRC_DIR/files_$1/$2
   else
     echo "--- vips dzsave $JOB_IN_DIR/$2.svs $ENV_LINK_SRC_DIR/files_$1/$2"
     vips dzsave $JOB_IN_DIR/$2.svs $ENV_LINK_SRC_DIR/files_$1/$2
